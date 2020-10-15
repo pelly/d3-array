fd77ba0acbde589dbd27ea42d22f16ed35388317
John Pelly <jpelly@gmail.com>
2020-10-15 09:47:52 -0400

    fixing cumsum.js to work around babel parsing bug

--- src/cumsum.js
+++ src/cumsum.js
@@ -1,4 +1,11 @@
 export default function cumsum(values, valueof) {
-  var sum = 0, index = 0;
-  return Float64Array.from(values, valueof === undefined ? v => (sum += +v || 0) : v => (sum += +valueof(v, index++, values) || 0));
+    var sum = 0, index = 0;
+    let fn = null;
+    if (valueof === undefined) {
+	fn = v => (sum += +v || 0)
+    } else {
+	fn = v => (sum += +valueof(v, index++, values) || 0)
+    }
+    
+  return Float64Array.from(values, );
 }
